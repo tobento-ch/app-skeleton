d28af6a5bb3ada3a80013d767d6413fe6bd0034d
TOBENTOch <info@tobento.ch>
2022-02-16 19:10:49 +0100

    corrected path

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Get and run the application.
 (require __DIR__.'/../app/app.php')->run();
