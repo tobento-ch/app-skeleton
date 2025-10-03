a80a566544041044e25f64c666ee1009b90ef3e1
TOBENTOch <info@tobento.ch>
2025-10-03 12:34:14 +0200

    restore error and exception handlers

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -22,4 +22,12 @@ class TestCase extends BaseTestCase
     {
         return require __DIR__.'/../app/app.php';
     }
+    
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        
+        restore_error_handler();
+        restore_exception_handler();
+    }
 }
